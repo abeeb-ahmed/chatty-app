31926337d93d170d4144645ac80a9e318dbc4bde
Abeeb Ahmed <abeebahmed@gmail.com>
2022-12-01 21:16:29 +0100

    bug fix

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -14,7 +14,7 @@ const Chat = () => {
   return (
     <div className="chat">
       <div className="chatInfo">
-        <span>{data.user?.displayName}</span>
+        <span>{data.user?.displayName || ""}</span>
         <div className="chatIcons">
           <img src={Cam} alt="" />
           <img src={Add} alt="" />
